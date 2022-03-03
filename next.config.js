7d5d32e219d025cb97d31f0de4018b1fecaa776d
Alessa <alessandra.messineo@voxmedia.com>
2022-03-03 15:47:40 -0500

    restyle nav bar. start summary style

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    OPENAI_API_KEY: "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",
+    OPENAI_API_KEY: "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",
     OPENAI_ORG: "org-W7xk4kbIBvOkaVFBPwKaQDgA"
   },
 };
