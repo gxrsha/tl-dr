d53d9a5b075b8ba6660674a9a7c8c06f2ae799ec
Alessa <alessandra.messineo@voxmedia.com>
2022-03-03 19:43:47 -0500

    add yellow dropshadow. adjust quote sizing

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    OPENAI_API_KEY: "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",
+    OPENAI_API_KEY: "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",
     OPENAI_ORG: "org-W7xk4kbIBvOkaVFBPwKaQDgA"
   },
 };
