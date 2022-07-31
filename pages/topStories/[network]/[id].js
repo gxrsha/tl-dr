5d1c91f1846770670f817e3efb748b010b3ea4a4
gxrsha <sammysirak@gmail.com>
2022-07-31 16:39:58 -0500

    removing console logs

--- pages/topStories/[network]/[id].js
+++ pages/topStories/[network]/[id].js
@@ -14,48 +14,52 @@ export const getServerSideProps = async (context) => {
   };
 };
 
-const articleSummaryPage = ({ title, url, networkImage }) => {
-  const [summary, setSummary] = useState()
+const ArticleSummaryPage = ({ title, url, networkImage }) => {
+  const [summary, setSummary] = useState();
 
   useEffect(() => {
     getArticleText(url).then((articleText) => {
       generateSummary(articleText).then((summaryResponse) => {
-          console.log('generateSummary response: ', summaryResponse);
-          const summary = summaryResponse.data.choices[0].text;
-          setSummary(summary);
-      })
-    })
+        const summary = summaryResponse.data.choices[0].text;
+        setSummary(summary);
+      });
+    });
   }, []);
 
-  const getArticleText = async(url) => {
-    const articleText = await extractTextFromArticle(url)
-    return articleText
+  const getArticleText = async (url) => {
+    const articleText = await extractTextFromArticle(url);
+    return articleText;
   };
 
-  const generateSummary = async(articleText) => {
-      const configuration = new Configuration({
-          organization: process.env.OPENAI_ORG,
-          apiKey: process.env.OPENAI_API_KEY
-        });
-        const openai = new OpenAIApi(configuration);
-        
-        const response = await openai.createCompletion("text-davinci-001", {
-          prompt: `Summarize this for a second-grade student:\n\n${articleText}`,
-          temperature: 1,
-          max_tokens: 500,
-          top_p: 1.0
-        });
+  const generateSummary = async (articleText) => {
+    const configuration = new Configuration({
+      // organization: process.env.OPENAI_ORG,
+      apiKey: process.env.OPEN_API_KEY_TEST,
+    });
+    const openai = new OpenAIApi(configuration);
 
-        return response;
+    const response = await openai.createCompletion("text-davinci-001", {
+      prompt: `Summarize this for a second-grade student:\n\n${articleText}`,
+      temperature: 1,
+      max_tokens: 500,
+      top_p: 1.0,
+    });
+
+    return response;
   };
 
   return (
     <>
       <main className={styles.main}>
-        <Summary title={title} url={url} networkImage={networkImage} summaryText={summary} />
+        <Summary
+          title={title}
+          url={url}
+          networkImage={networkImage}
+          summaryText={summary}
+        />
       </main>
     </>
   );
 };
 
-export default articleSummaryPage;
+export default ArticleSummaryPage;
