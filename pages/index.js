b64073ad193eb68ec5837adcd429966d49faa926
gxrsha <sammysirak@gmail.com>
2022-07-31 16:37:52 -0500

    adding landing page to index

--- pages/index.js
+++ pages/index.js
@@ -11,18 +11,24 @@ import {
 } from "../common/mediaData";
 
 export default function Home() {
+  console.log("env variable: ", process.env.NEXT_PUBLIC_TEST);
 
   return (
     <div className={styles.container}>
       <Head>
         <title>TL;DR</title>
-        <meta name="description" content="TL;DR - Summarize your favorite vox media articles" />
+        <meta
+          name="description"
+          content="TL;DR - Summarize your favorite vox media articles"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
         <h1 className="flex flex-col tracking-[0.3rem] font-display font-bold px-10 text-3xl text-center mb-5">
           tl;dr
-          <span className="font-extralight font-sans mt-3 text-base tracking-wide">[ Summarize articles from your favorite networks ]</span>
+          <span className="font-extralight font-sans mt-3 text-base tracking-wide">
+            [ Summarize articles from your favorite networks ]
+          </span>
         </h1>
         <MediaSite mediaObj={voxDataObj} />
         <MediaSite mediaObj={theVergeDataObj} />
