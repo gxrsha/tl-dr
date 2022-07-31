e30e109e40abe67a42bf67e26849819444c17583
gxrsha <sammysirak@gmail.com>
2022-07-31 13:38:12 -0500

    Cleaning up unused code and made Summary page prettier

--- components/Summary.jsx
+++ components/Summary.jsx
@@ -1,32 +1,36 @@
-import { useEffect, useState } from "react";
-import { getTopStories } from "../pages/api/scraper";
-import Skeleton from "react-loading-skeleton";
-import 'react-loading-skeleton/dist/skeleton.css'
-import Link from 'next/link'
+import "react-loading-skeleton/dist/skeleton.css";
+import { LoadingSpinner } from "../components";
 
 const Summary = ({ title, url, networkImage, summaryText }) => {
-//   const [topStories, setTopStories] = useState([]);
-//   const [isLoading, setIsLoading] = useState(true)
-  
-
-  useEffect(() => {
-//     const retrieveSiteInfo = async () => {
-//       const topStories = await getTopStories(url);
-//       setTopStories(topStories);
-//       setIsLoading(false)
-//     };
-
-//     retrieveSiteInfo();
-  }, []);
+  const navigateToUrl = (url) => {
+    console.log("this is our url: ", url);
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
 
   return (
-      <div>
-        <img src={`${networkImage}`} className="p-[4px] w-20 h-20 object-cover"/>
-        <h1>{title}</h1><br/>
-        <p>{summaryText}</p><br/>
-        <p>Read more <a href={url} target="_blank">here</a></p>
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex items-center px-8 mb-8">
+        <img src={`${networkImage}`} className="w-20 h-20 object-cover" />
+        <h1 className="ml-4 font-bold text-xl md:text-2xl">{title}</h1>
       </div>
-  )
-}
+      {!summaryText && <LoadingSpinner />}
+      <div className="mb-10 flex justify-center">
+        <p className="font-extralight font-sans mt-3 text-base px-8 md:w-1/2">
+          {summaryText}
+        </p>
+      </div>
+      <div className="flex md:justify-end md:w-1/2 px-12">
+        {summaryText && (
+          <button
+            onClick={() => navigateToUrl(url)}
+            className="rounded-full bg-slate-900 text-stone-100 py-[5px] px-[30px]"
+          >
+            Read More Here
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Summary;
