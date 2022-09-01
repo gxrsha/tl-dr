829e9fac1fff17c6f030e4143afaf291dcd1c2a9
Sammy Sirak <sammy.sirak@voxmedia.com>
2022-09-01 11:43:45 -0500

    Removing unused function

--- pages/api/scraper.js
+++ pages/api/scraper.js
@@ -35,28 +35,7 @@ export default async (req, res) => {
   }
 }
 
-
 const getWebsiteData = async (url) => {
   const response = await axios.get(url);
   return response.data;
 };
-
-export const extractTextFromArticle = async (url) => {
-  const articleText = []
-
-  const html = await getWebsiteData(url)
-  const soup = new JSSoup(html)
-
-  const articleContent = soup.find('div', 'c-entry-content')
-  const paragraphTags = articleContent.findAll('p')
-
-  // Due to token limitations we only want the first 10 paragraphs
-  const firstTenParagraphs = paragraphTags.slice(0, 10)
-
-  firstTenParagraphs.forEach((paragraph) => {
-    articleText.push(paragraph.text)
-  })
-
-  return articleText.join(' ')
-
-}
