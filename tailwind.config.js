fa80de5064ea34417d72c8cb2036ee18986978da
Sammy Sirak <sammy.sirak@voxmedia.com>
2022-09-06 10:57:49 -0500

    making some additional changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-    mode: 'jit',
     content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    purge: [
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
     ],
